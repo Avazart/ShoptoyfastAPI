5e8c48827336fd5d22bcf5d263d6305f522a531c
Yana <mariablossom713@gmail.com>
2025-01-31 21:07:29 +0200

    Fix import decimal

--- src/common/dto/product.py
+++ src/common/dto/product.py
@@ -1,4 +1,4 @@
-from _decimal import Decimal
+from decimal import Decimal
 from typing import Annotated, Any
 
 from pydantic import BaseModel, Field
--- src/database/repositories/product.py
+++ src/database/repositories/product.py
@@ -1,4 +1,4 @@
-from _decimal import Decimal
+from decimal import Decimal
 from typing import Annotated
 
 from pydantic import Field
