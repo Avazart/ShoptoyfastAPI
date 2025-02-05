8fa9515a58c6073dc4056be259ac14e70f254799
Avazart <avazart7@gmail.com>
2025-02-05 12:30:38 +0200

    Split into functions

---
+++ tests/conftest.py
@@ -0,0 +1,47 @@
+from collections.abc import AsyncGenerator
+
+import pytest
+from httpx import ASGITransport, AsyncClient
+from sqlalchemy.ext.asyncio import (
+    AsyncSession,
+    async_sessionmaker,
+    create_async_engine,
+)
+
+from src.__main__ import app
+from src.core.session import get_session
+from src.database.models.base import Base
+
+DATABASE_URL = "sqlite+aiosqlite:///:memory:"
+BASE_URL = "http://127.0.0.1:8000"
+
+engine = create_async_engine(DATABASE_URL)
+TestingSessionLocal = async_sessionmaker(
+    engine, class_=AsyncSession, expire_on_commit=False
+)
+
+async def get_test_session() -> AsyncGenerator[AsyncSession, None]:
+    async with TestingSessionLocal() as session:
+        yield session
+
+
+app.dependency_overrides[get_session] = get_test_session
+
+
+@pytest.fixture(autouse=True, scope="function")
+async def init_session() -> AsyncGenerator[AsyncSession, None]:
+    async with TestingSessionLocal() as session:
+        async with engine.begin() as connection:
+            await connection.run_sync(Base.metadata.create_all)
+        yield session
+        async with engine.begin() as connection:
+            await connection.run_sync(Base.metadata.drop_all)
+
+
+@pytest.fixture(scope="function")
+async def test_client():
+    async with AsyncClient(
+            transport=ASGITransport(app=app),
+            base_url=BASE_URL
+    ) as client:
+        yield client
--- tests/test_categories.py
+++ tests/test_categories.py
@@ -1,74 +1,62 @@
-from collections.abc import AsyncGenerator
-
-import pytest
-from httpx import ASGITransport, AsyncClient
-from sqlalchemy.ext.asyncio import (
-    AsyncSession,
-    async_sessionmaker,
-    create_async_engine,
-)
-from sqlalchemy.pool import StaticPool
-
-from src.__main__ import app
 from src.common.dto.category import CategoryDTO
-from src.core.session import get_session
-from src.database.models.base import Base
 
-DATABASE_URL = "sqlite+aiosqlite:///:memory:"
-BASE_URL = "http://127.0.0.1:8000"
+from .conftest import test_client # noqa
 
-engine = create_async_engine(
-    DATABASE_URL,
-    echo=False,
-    connect_args={"check_same_thread": False},
-    poolclass=StaticPool,
-)
-
-
-@pytest.fixture()
-async def test_client():
-    async with AsyncClient(
-        transport=ASGITransport(app=app),
-        base_url=BASE_URL,
-    ) as client:
-        yield client
-
-
-async def get_test_session() -> AsyncGenerator[AsyncSession, None]:
-    TestingSessionLocal = async_sessionmaker(
-        engine, class_=AsyncSession, expire_on_commit=False
-    )
-    async with TestingSessionLocal() as session:
-        async with engine.begin() as connection:
-            await connection.run_sync(Base.metadata.create_all)
-        yield session
-
-
-app.dependency_overrides[get_session] = get_test_session
-
-
-async def test_categories(test_client):
-    cat = CategoryDTO(name="test_category")
+async def create_category(name: str, test_client):
+    cat = CategoryDTO(name=name)
     response = await test_client.post("/categories", json=cat.model_dump())
     assert response.status_code == 200
     json_content = response.json()
     assert isinstance(json_content, dict)
     assert json_content["name"] == cat.name
+    return json_content["id"]
 
-    response = await test_client.get("/categories")
-    assert response.status_code == 200
-    json_content = response.json()
-    assert isinstance(json_content, list)
-    assert len(json_content) == 1
-    assert json_content[0]["name"] == cat.name
-    category_id = json_content[0]["id"]
+
+async def test_create_category(test_client):
+    await create_category("test_category", test_client)
+
+
+async def test_get_category(test_client):
+    category_id = await create_category("test_category", test_client)
 
     response = await test_client.get(f"/categories/{category_id}")
     assert response.status_code == 200
     json_content = response.json()
     assert isinstance(json_content, dict)
-    assert json_content["name"] == cat.name
     assert json_content["id"] == category_id
+
+
+async def test_get_categories(test_client):
+    category_ids = {
+        await create_category(f"test_category_{i}", test_client)
+        for i in range(10)
+    }
+    response = await test_client.get("/categories")
+    assert response.status_code == 200
+    json_content = response.json()
+    assert isinstance(json_content, list)
+    assert len(json_content) == len(category_ids)
+    assert {c["id"] for c in json_content} == category_ids
+
+async def test_get_categories_with_query_params(test_client):
+    category_ids = [
+        await create_category(f"test_category_{i}", test_client)
+        for i in range(15)
+    ]
+    offset = 7
+    limit = 5
+    params = dict(offset=offset, limit=limit)
+    response = await test_client.get("/categories", params=params)
+    assert response.status_code == 200
+    json_content = response.json()
+    assert isinstance(json_content, list)
+    assert len(json_content) == limit
+    expected = category_ids[offset:offset+limit]
+    assert {c["id"] for c in json_content} == set(expected)
+
+
+async def test_delete_category(test_client):
+    category_id = await create_category("test_category", test_client)
 
     response = await test_client.delete(f"/categories/{category_id}")
     assert response.status_code == 200
